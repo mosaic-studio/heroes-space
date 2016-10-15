077431ed81716997ad37a835866aa3abc1e92669
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2016-10-15 20:22:54 -0300

    Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- heroes_space/static/webpage/js/Game/fase01/Game.js
+++ heroes_space/static/webpage/js/Game/fase01/Game.js
@@ -16,59 +16,61 @@ BasicGame.Game = function (game) {
 BasicGame.Game.prototype = {
 
     init: function () {
-        // set up input max pointers
-        this.input.maxPointers = 1;
-        // set up stage disable visibility change
-        this.stage.disableVisibilityChange = true;
-        // Set up the scaling method used by the ScaleManager
-        // Valid values for scaleMode are:
-        // * EXACT_FIT
-        // * NO_SCALE
-        // * SHOW_ALL
-        // * RESIZE
-        // See http://docs.phaser.io/Phaser.ScaleManager.html for full document
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        // If you wish to align your game in the middle of the page then you can
-        // set this value to true. It will place a re-calculated margin-left
-        // pixel value onto the canvas element which is updated on orientation /
-        // resizing events. It doesn't care about any other DOM element that may
-        // be on the page, it literally just sets the margin.
-        this.scale.pageAlignHorizontally = true;
-        this.scale.pageAlignVertically = true;
-        // Force the orientation in landscape or portrait.
-        // * Set first to true to force landscape.
-        // * Set second to true to force portrait.
-        this.scale.forceOrientation(false, true);
-        // Sets the callback that will be called when the window resize event
-        // occurs, or if set the parent container changes dimensions. Use this
-        // to handle responsive game layout options. Note that the callback will
-        // only be called if the ScaleManager.scaleMode is set to RESIZE.
-        this.scale.setResizeCallback(this.gameResized, this);
-        // Set screen size automatically based on the scaleMode. This is only
-        // needed if ScaleMode is not set to RESIZE.
-        this.scale.updateLayout(true);
-        // Re-calculate scale mode and update screen size. This only applies if
-        // ScaleMode is not set to RESIZE.
-        this.scale.refresh();
+        /*// set up input max pointers
+         this.input.maxPointers = 1;
+         // set up stage disable visibility change
+         this.stage.disableVisibilityChange = true;
+         // Set up the scaling method used by the ScaleManager
+         // Valid values for scaleMode are:
+         // * EXACT_FIT
+         // * NO_SCALE
+         // * SHOW_ALL
+         // * RESIZE
+         // See http://docs.phaser.io/Phaser.ScaleManager.html for full document
+         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+         // If you wish to align your game in the middle of the page then you can
+         // set this value to true. It will place a re-calculated margin-left
+         // pixel value onto the canvas element which is updated on orientation /
+         // resizing events. It doesn't care about any other DOM element that may
+         // be on the page, it literally just sets the margin.
+         this.scale.pageAlignHorizontally = true;
+         this.scale.pageAlignVertically = true;
+         // Force the orientation in landscape or portrait.
+         // * Set first to true to force landscape.
+         // * Set second to true to force portrait.
+         this.scale.forceOrientation(false, true);
+         // Sets the callback that will be called when the window resize event
+         // occurs, or if set the parent container changes dimensions. Use this
+         // to handle responsive game layout options. Note that the callback will
+         // only be called if the ScaleManager.scaleMode is set to RESIZE.
+         this.scale.setResizeCallback(this.gameResized, this);
+         // Set screen size automatically based on the scaleMode. This is only
+         // needed if ScaleMode is not set to RESIZE.
+         this.scale.updateLayout(true);
+         // Re-calculate scale mode and update screen size. This only applies if
+         // ScaleMode is not set to RESIZE.
+         this.scale.refresh();*/
+
+        this.bulletTime = 0;
 
     },
 
     preload: function () {
-        this.load.image('space', 'assets/skies/deep-space.jpg');
-        this.load.image('bullet', 'assets/games/asteroids/bullets.png');
-        this.load.image('ship', 'assets/games/asteroids/ship.png');
+        this.load.image('space', 'static/webpage/assets/Backgrounds/purple.png');
+        this.load.image('bullet', 'static/webpage/assets/Lasers/laserBlue01.png');
+        this.load.image('ship', 'static/webpage/assets/Ships/playerShip1_blue.png');
     },
 
     create: function () {
         //  This will run in Canvas mode, so let's gain a little speed and display
-        this.renderer.clearBeforeRender = false;
-        this.renderer.roundPixels = true;
+        this.game.renderer.clearBeforeRender = false;
+        this.game.renderer.roundPixels = true;
 
         //  We need arcade physics
-        this.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
         //  A spacey background
-        this.add.tileSprite(0, 0, this.width, this.height, 'space');
+        this.add.tileSprite(0, 0, this.game.width, this.game.height, 'space');
 
         //  Our ships bullets
         this.bullets = this.add.group();
@@ -85,7 +87,7 @@ BasicGame.Game.prototype = {
         this.sprite.anchor.set(0.5);
 
         //  and its physics settings
-        this.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+        this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 
         this.sprite.body.drag.set(100);
         this.sprite.body.maxVelocity.set(200);
@@ -94,14 +96,12 @@ BasicGame.Game.prototype = {
         this.cursors = this.input.keyboard.createCursorKeys();
         this.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
 
-        this.bulletTime = 0;
-
     },
 
     update: function(){
         if (this.cursors.up.isDown)
         {
-            this.physics.arcade.accelerationFromRotation(sprite.rotation, 200, sprite.body.acceleration);
+            this.game.physics.arcade.accelerationFromRotation(this.sprite.rotation, 200, this.sprite.body.acceleration);
         }
         else
         {
@@ -143,42 +143,40 @@ BasicGame.Game.prototype = {
 
     fireBullet: function () {
 
-        if (game.time.now > bulletTime)
+        if (this.time.now > this.bulletTime)
         {
-            bullet = bullets.getFirstExists(false);
+            this.bullet = this.bullets.getFirstExists(false);
 
-            if (bullet)
+            if (this.bullet)
             {
-                bullet.reset(sprite.body.x + 16, sprite.body.y + 16);
-                bullet.lifespan = 2000;
-                bullet.rotation = sprite.rotation;
-                game.physics.arcade.velocityFromRotation(sprite.rotation, 400, bullet.body.velocity);
-                bulletTime = game.time.now + 50;
+                this.bullet.reset(this.sprite.body.x + 16, this.sprite.body.y + 16);
+                this.bullet.lifespan = 2000;
+                this.bullet.rotation = this.sprite.rotation;
+                this.physics.arcade.velocityFromRotation(this.sprite.rotation, 400, this.bullet.body.velocity);
+                this.bulletTime = this.time.now + 50;
             }
         }
-
     },
 
     screenWrap: function(sprite) {
 
         if (sprite.x < 0)
         {
-            sprite.x = game.width;
+            sprite.x = this.game.width;
         }
-        else if (sprite.x > game.width)
+        else if (sprite.x >  this.game.width)
         {
             sprite.x = 0;
         }
-
         if (sprite.y < 0)
         {
-            sprite.y = game.height;
+            sprite.y = this.game.height;
         }
-        else if (sprite.y > game.height)
+        else if (sprite.y > this.game.height)
         {
             sprite.y = 0;
         }
 
-    },
+    }
 
 };
