60255347084f92de85f64efd3ff3ce0c5366a104
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2016-10-18 22:33:53 -0200

    Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- heroes_space/static/webpage/js/scripts/Level01.js
+++ heroes_space/static/webpage/js/scripts/Level01.js
@@ -13,9 +13,11 @@ BasicGame.Level01.prototype = {
 
     init: function () {
         this.bulletTime = 0;
+        this.bulletSpeed = 10;
         this.lives = 3;
         this.maxHealthShip = this.actualHealthShip = 100;
         this.actualPoints = 0;
+        this.gun = new Phaser.Point();
     },
 
     preload: function () {
@@ -32,6 +34,7 @@ BasicGame.Level01.prototype = {
             m.body.setCircle(60);
             m.body.setCollisionGroup(this.meteorCollisionGroup);
             m.body.collides([this.meteorCollisionGroup, this.playerCollisionGroup]);
+            m.body.collides( this.bulletsCollisionGroup);
         }
     },
 
@@ -39,7 +42,7 @@ BasicGame.Level01.prototype = {
         //  Our player ship
         this.ship = this.add.sprite(99, 75, 'ship');
         this.ship.smoothed = true;
-        this.game.physics.p2.enable(this.ship, true);
+        this.game.physics.p2.enable(this.ship, false);
         this.ship.body.setRectangle(80, 50);
         this.ship.body.setCollisionGroup(this.playerCollisionGroup);
         this.ship.body.collides(this.meteorCollisionGroup, this.shipHitMeteor, this);
@@ -51,12 +54,27 @@ BasicGame.Level01.prototype = {
         //  Our ships bullets
         this.bullets = this.add.group();
         this.bullets.enableBody = true;
-        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        this.bullets.physicsBodyType = Phaser.Physics.P2JS;
 
-        //  All 40 of them
-        this.bullets.createMultiple(40, 'bullet');
-        this.bullets.setAll('anchor.x', 1);
+        //  All 50 of them
+        this.bullets.createMultiple(50, 'bullet');
+        this.bullets.setAll('anchor.x', 0.5);
         this.bullets.setAll('anchor.y', 0.5);
+        this.bullets.setAll('outOfBoundsKill', true);
+        this.bullets.setAll('checkWorldBounds', true);
+
+
+        this.bullets.forEach(function(bullet){
+            bullet.body.setRectangle(50, 5);
+            bullet.body.fixedRotation = true;
+            bullet.body.setCollisionGroup(this.bulletsCollisionGroup);
+            bullet.body.collides(this.meteorCollisionGroup, this.hit, this);
+        }, this, false);
+
+    },
+
+    hit: function(bullet, meteor){
+        meteor.sprite.kill();
     },
 
     create: function () {
@@ -70,6 +88,7 @@ BasicGame.Level01.prototype = {
 
         this.playerCollisionGroup = this.game.physics.p2.createCollisionGroup();
         this.meteorCollisionGroup = this.game.physics.p2.createCollisionGroup();
+        this.bulletsCollisionGroup = this.game.physics.p2.createCollisionGroup();
 
         //  This part is vital if you want the objects with their own collision groups to still collide with the world bounds
         //  (which we do) - what this does is adjust the bounds to use its own collision group.
@@ -83,20 +102,21 @@ BasicGame.Level01.prototype = {
 
         this.createMeteors();
 
-        // this.createBullets();
-
         this.createShip();
+
+        this.createBullets();
 
         //  scripts input
         this.cursors = this.input.keyboard.createCursorKeys();
-        // this.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
+        this.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
 
         this.loadUI();
 
     },
 
     update: function(){
-        this.game.physics.arcade.collide(this.ship, this.meteors);
+        // this.game.physics.arcade.collide(this.ship, this.meteors);
+
         if (this.cursors.up.isDown)
         {
             this.ship.body.thrust(400);
@@ -129,18 +149,18 @@ BasicGame.Level01.prototype = {
             this.space.tilePosition.y -= (this.ship.body.velocity.y * this.game.time.physicsElapsed);
         }
 
-        // if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
-        // {
-        //    this.fireBullet();
-        // }
+        if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+        {
+            this.fireBullet();
+        }
 
         // this.screenWrap(this.ship);
 
-        // this.bullets.forEachExists(this.screenWrap, this);
+        //this.bullets.forEachExists(this);
     },
 
 
-    shipHitMeteor: function (body1, body2) {
+    shipHitMeteor: function (ship, meteor) {
         this.actualHealthShip =- 1;
         this.myHealthBar.setPercent(this.actualHealthShip);
         //body2.removeFromWorld();
@@ -150,16 +170,19 @@ BasicGame.Level01.prototype = {
 
         if (this.time.now > this.bulletTime)
         {
-            this.bullet = this.bullets.getFirstExists(false);
+            var nextBullet = this.bullets.getFirstExists(false);
 
-            if (this.bullet)
+            if (nextBullet)
             {
-                this.bullet.reset(this.ship.centerX, this.ship.centerY);
-                this.bullet.lifespan = 2000;
-                this.bullet.rotation = this.ship.rotation;
-                this.physics.arcade.velocityFromRotation(this.ship.rotation, 600, this.bullet.body.velocity);
-                // this.bullet.body.velocity.copyFrom(this.game.physics.arcade.velocityFromAngle(this.ship.angle, 700));
-
+                this.gun.setTo(0, this.ship.height*-1);
+                this.gun.rotate(1, 2, this.ship.rotation);
+                nextBullet.reset(this.ship.body.x + this.gun.x, this.ship.body.y + this.gun.y);
+                nextBullet.rotation = this.ship.rotation;
+                // nextBullet.body.velocity.x = Math.cos(nextBullet.rotation) * this.bulletSpeed + this.ship.body.velocity.x;
+                // nextBullet.body.velocity.y = Math.sin(nextBullet.rotation) * this.bulletSpeed + this.ship.body.velocity.y;
+                nextBullet.body.velocity.x = this.gun.x * this.bulletSpeed;
+                nextBullet.body.velocity.y = this.gun.y * this.bulletSpeed;
+                nextBullet.lifespan = 2000;
                 this.bulletTime = this.time.now + 50;
             }
         }
